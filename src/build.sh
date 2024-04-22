89a06ff4e3b8ebd824e2f850feb1b6ad3f4b482c
Derek Murphy <derek@jstar.ai>
2024-04-22 15:10:38 -0600

    - Add archive build for release

--- src/build.sh
+++ src/build.sh
@@ -3,16 +3,16 @@
 exe_ver="1.0"
 exe_name="LightSimulator"
 build_date="$(date "+%Y-%m-%d %H:%M:%S")"
+build_archive="true"
 
 function DoBuild()
 {
   export GOOS="${1}"
   export GOARCH="${2}"
 
-  local out_suffix="_${1}"
+  local out_suffix="_${1}_${2}"
   local out_ext=""
   if [ "${1}" == "windows" ]; then
-    out_suffix=""
     out_ext=".exe"
   fi
   local out_filename="${exe_name}${out_suffix}${out_ext}"
@@ -26,5 +26,12 @@ function DoBuild()
 DoBuild "windows" "amd64"
 DoBuild "linux" "amd64"
 DoBuild "darwin" "amd64"
+
+if [ "${build_archive}" == "true" ]; then
+  archive="${exe_name}_${exe_ver}.tar.gz"
+  echo "Creating ${archive}"
+  [ -f "./${archive}" ] && rm -rf "./${archive}"
+  COPYFILE_DISABLE=1 tar cvzf "./${archive}" ./${exe_name}* ./*.json ./public_html/*
+fi
 
 echo "DONE!"
